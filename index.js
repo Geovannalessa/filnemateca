e7e3af1252ec453eb0f5b6659a9495fd48d429d5
Geovanna Lessa <geovannalessa2@gmail.com>
2022-01-20 17:15:30 -0300

    criado a rota para capturar as informações de um filme pela posição

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ servidor.get('/', (req, res) => {
         res.send('Está aí o que você queria.. nn me abandone me teste again later :)')
     })
     //por o servidor para 'ouvir' as requisiçoes
-servidor.get('/filme', (req, res) => {
-    console.log('outro servidor filme sendo usado..');
-    res.send("Meu último filme q vi foi o primeiro Mortal Kombat ;3")
+servidor.get('/filme/:posicao', (req, res) => {
+    console.log('esta sendo usado')
+        //capturei a posiça~o do filme desejado
+    let posicao = req.params.posicao;
+
+    //carregar o array de filmes 
+    const filmes = require('./database/filmes.json');
+
+    //enviar para o cliente o filme da posicao desejada
+    res.send(filmes[posicao])
 })
+
+
 servidor.get('/generos', (req, res) => {
     console.log('servidor de generos funcionando..');
     res.send('ola aqui ainda nn tem nenhum gêneros hehe');
@@ -24,7 +33,8 @@ servidor.get('/generos', (req, res) => {
 servidor.listen(3000);
 
 
+
 //informações sobre o res --> 
-//tem o .get envia os dados para o bowser e precisa de dois parâmetros uma solicitação e uma resposta;
+//tem o .get envia os dados para o bowser e precisa de dois parâmetros a rota o que deve fazer após. Uma solicitação e uma resposta;
 //.post para criar informações;
 //.sendFile que envia arquivos existentes;Utiliza o path para criar uma rota(ele precisa ser importado, em uma const); e usar o .join() para mostrar o caminho; __dirname é uma constante node.js que se refere ao diretório que está sendo executado.
