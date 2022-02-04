e0c76fabaf96ede66e41972ed7951d6b977e259a
Geovanna Lessa <geovannalessa2@gmail.com>
2022-02-03 22:06:10 -0300

    Duas novas rotas para o servidor: Buscar pelo id do filme e buscar pelo gênero do filme

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ servidor.get('/generos', (req, res) => {
     console.log('servidor de generos funcionando..');
     res.send('ola aqui ainda nn tem nenhum gêneros hehe');
 })
-
-//'/filme/posicao' => pega o filme o pela posição que o cliente for solicitar ao pesquisar.
 servidor.get('/filme/:posicao', (req, res) => {
     console.log('esta sendo usado')
         //capturei a posiça~o do filme desejado
@@ -29,9 +27,7 @@ servidor.get('/filme/:posicao', (req, res) => {
     //enviar para o cliente o filme da posicao desejada
     res.send(filmes[posicao])
 })
-
-//'/busca/:trecho' => irá permitir que o cliente busque o título do filme pela barra de pesquisa e ter resultados.b
-servidor.get('/busca/:trecho', (req, res) => {
+servidor.get('/buscar/:trecho', (req, res) => {
     console.log('usando o servidor busca..');
 
     // 1: Salvar o trecho buscado na variável 'trecho';
@@ -44,15 +40,31 @@ servidor.get('/busca/:trecho', (req, res) => {
     // 3: Filtrar do array filmes, somente os filmes que possuam o trecho no titulo. => (lembrem da função filmes.filter)
 
     const filmesTitulo = filmes.filter(function(valorDaBusca) {
-        return valorDaBusca.titulo.includes(trecho);
-    })
-
-    //nesta constante eu fiz com que o cliente que digita-se qualquer coisa na barra de pesquisa retornasse os nomes dos titulos conrrespondentes. e como?
-    //na contaste eu fiz com que recebe-se todos filmes só que ultilizando o filter com uma função recebendo como parametro um nome que vai ser por onde a funçao vai funcionar, e fiz com q retorna-se o parametro+todos os títulos dos filmes mas apenas as letras que o cliente for digitar na barra de pesquisa
+            return valorDaBusca.titulo.includes(trecho);
+        })
+        //nesta constante eu fiz com que o cliente que digita-se qualquer coisa na barra de pesquisa retornasse os nomes dos titulos conrrespondentes. e como?
+        //na contaste eu fiz com que recebe-se todos filmes só que ultilizando o filter com uma função recebendo como parametro um nome que vai ser por onde a funçao vai funcionar, e fiz com q retorna-se o parametro+todos os títulos dos filmes mas apenas as letras que o cliente for digitar na barra de pesquisa
 
     // 4: Enviar para o cliente(usando res.send) o resultado da filtragem.
     res.send(filmesTitulo)
-        // res.send(req.params.trecho);
+
+    // res.send(req.params.trecho);
+})
+servidor.get('/buscarid/:trechoId', (req, res) => {
+    console.log('servidor de busca por id está funcionando.....');
+    let trechoId = req.params.trechoId;
+    const filmes = require('./database/filmes.json');
+    const filmesId = filmes.filter(function(idFilme) {
+        return idFilme.id.includes(trechoId)
+    })
+    res.send(filmesId)
+});
+servidor.get('/buscargeneros/:trechoGeneros', (req, res) => {
+    console.log('servidor gêneros está sendo usado! ;)');
+    let trechoGeneros = req.params.trechoGeneros;
+    const filmes = require('./database/filmes.json');
+    const filmesGeneros = filmes.filter((genero) => genero.generos.includes(trechoGeneros))
+    res.send(filmesGeneros);
 })
 servidor.listen(3000);
 //como colocar a sinopse de todos os filmes?
