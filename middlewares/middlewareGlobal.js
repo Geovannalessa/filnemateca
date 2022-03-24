ecf524bcf3d47b490680a8226dddd1d03c33322a
Geovanna Lessa <geovannalessa2@gmail.com>
2022-03-24 16:17:08 -0300

    Tratando res e req na entrada e na saída no middleware global

--- middlewares/middlewareGlobal.js
+++ middlewares/middlewareGlobal.js
@@ -4,6 +4,8 @@ module.exports = (req, res, next) => {
     if (hora < 8 || hora >= 20) {
         res.send("Já está tarde.. vá para su casa");
     } else {
+        console.log('next indo')
         next();
+        console.log('next voltando')
     }
 };
