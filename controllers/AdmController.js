c0481ac1b38a8fc61b09e5acfb498b84a79d7de2
Geovanna Lessa <geovannalessa2@gmail.com>
2022-03-28 18:18:16 -0300

    Adicionado uma nova função que direcionará o usuario que for cadastrado

--- controllers/AdmController.js
+++ controllers/AdmController.js
@@ -1,6 +1,22 @@
 module.exports = {
+    mostralogin: (req, res) => {
+        res.render('login.ejs');
+    },
     login: (req, res) => {
-        console.log("modo adm ativado");
-        res.render('login.ejs')
+        //carregar o array de usuarios
+        const usuarios = require("../database/usuarios.json");
+
+        //capturar o email e a senha digitados pelo visitante
+        let dados = req.body;
+        res.send(dados);
+        //verificar se usuario é cadastrado
+
+        //procuro no array de ususarios se eciste algum com login e senha digitados
+
+        //caso exista o usuario com o email e senha dados, retornar OK!
+        //caso não exista, dar mensagem, "usuario inexistente"
+
+
+        res.send('ola again aqui é a página msm de login')
     }
 }
